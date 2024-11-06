ec1bb6798b6db60c10935ab6ff4dd7bb03030393
Dicki Maulana <bukandicki@gmail.com>
2024-11-06 20:11:59 +0700

    style: update project card styling

--- components/ProjectCard/index.vue
+++ components/ProjectCard/index.vue
@@ -1,3 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import "./ProjectCard.styles.scss";
+</script>
 
-<template></template>
+<template>
+  <div class="ProjectCard">
+    <NuxtImg src="/sample-2.png" class="ProjectCard__img" />
+
+    <div class="ProjectCard__detail">
+      <span>Dry Cleaning</span>
+      <strong>Jeans</strong>
+      <p>$ 10.00/pc</p>
+    </div>
+  </div>
+</template>
