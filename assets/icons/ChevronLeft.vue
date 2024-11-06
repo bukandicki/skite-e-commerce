b0e93de2fcde4dcd2807c0c3f4b4edd5ac550f83
Dicki Maulana <bukandicki@gmail.com>
2024-11-06 21:59:41 +0700

    feat: add back button on customer layout

---
+++ assets/icons/ChevronLeft.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="59"
+    height="59"
+    viewBox="0 0 59 59"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M20.3746 29.5L35.1492 44.2746L38.6253 40.7985L27.317 29.4902L38.6253 18.1818L35.1492 14.7254L20.3746 29.5Z"
+      fill="#2D9CDB"
+    />
+  </svg>
+</template>
--- layouts/customer.vue
+++ layouts/customer.vue
@@ -1,7 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ChevronLeft from "~/assets/icons/ChevronLeft.vue";
+
+const router = useRouter();
+const route = useRoute();
+</script>
 
 <template>
   <div class="CustomerLayout">
+    <button
+      v-if="route.name !== 'CustomerPage'"
+      class="CustomerLayout__back"
+      @click="() => router.back()"
+    >
+      <ChevronLeft />
+    </button>
+
     <slot />
   </div>
 </template>
