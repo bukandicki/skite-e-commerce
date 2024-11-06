9c461e657864f7dc88a95e8ac7b8820ff1f25dab
Dicki Maulana <bukandicki@gmail.com>
2024-11-06 20:27:14 +0700

    feat: define project card props

--- components/ProjectCard/index.vue
+++ components/ProjectCard/index.vue
@@ -1,15 +1,22 @@
 <script lang="ts" setup>
 import "./ProjectCard.styles.scss";
+
+const props = defineProps<{
+  label?: string;
+  title: string;
+  img: string;
+  subtitle: string;
+}>();
 </script>
 
 <template>
   <div class="ProjectCard">
-    <NuxtImg src="/sample-2.png" class="ProjectCard__img" />
+    <NuxtImg :src="img" class="ProjectCard__img" />
 
-    <div class="ProjectCard__detail">
-      <span>Dry Cleaning</span>
-      <strong>Jeans</strong>
-      <p>$ 10.00/pc</p>
+    <div class="ProjectCard__detail" :data-labeled="!!label">
+      <span v-if="label">{{ label }}</span>
+      <strong>{{ title }}</strong>
+      <p>{{ subtitle }}</p>
     </div>
   </div>
 </template>
