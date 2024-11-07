682a5f0ef8fe55a281aae7daf09f27b96387fd89
Dicki Maulana <bukandicki@gmail.com>
2024-11-07 23:47:50 +0700

    feat: create input component

---
+++ components/Input/index.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import type { InputTypeHTMLAttribute } from "vue";
+
+const props = defineProps<{
+  label?: string;
+  value?: any;
+  type?: InputTypeHTMLAttribute;
+  name: string;
+}>();
+
+const emit = defineEmits<{ onChange: [value?: any] }>();
+
+const handleChange = (e: Event) => {
+  const el = e.target as HTMLInputElement;
+
+  emit("onChange", el.value);
+};
+
+const handleInput = (e: Event) => {
+  if (props.type === "number") {
+    const el = e.target as HTMLInputElement;
+
+    el.value = el.value.replace(/[^0-9]/g, "");
+  }
+};
+</script>
+
+<template>
+  <div class="Input">
+    <label class="Input__label" :for="name">{{ label }}</label>
+
+    <input
+      @change="handleChange"
+      @input="handleInput"
+      class="Input__field"
+      :value="value"
+      :type="type"
+      :id="name"
+      :name="name"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+@use "./Input.styles.scss" as *;
+</style>
