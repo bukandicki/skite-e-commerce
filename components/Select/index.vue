9728a3c495d0476968b097c050a89534631ba21c
Dicki Maulana <bukandicki@gmail.com>
2024-11-07 17:37:49 +0700

    feat: select option component

--- components/Select/index.vue
+++ components/Select/index.vue
@@ -4,12 +4,20 @@ const props = defineProps<{
   value: T;
   options: Array<{ label: string; value: T }>;
 }>();
+
+const emit = defineEmits<{ change: [value: T] }>();
+
+const show = ref<boolean>(false);
 </script>
 
 <template>
-  <div class="Select">
-    <button class="Select__trigger">
-      {{ placeholder ? placeholder : "Select option" }}
+  <div :class="['Select', { 'Select--show': show }]">
+    <button @click="show = !show" class="Select__trigger">
+      <span>
+        {{ placeholder ? placeholder : "Select option" }}
+      </span>
+
+      <LazyIconChevronDown />
     </button>
 
     <ul class="Select__options">
@@ -18,7 +26,12 @@ const props = defineProps<{
         class="Select__item"
         :key="(option.value as string)"
       >
-        <button>{{ option.label }}</button>
+        <button
+          @click="() => emit('change', option.value)"
+          :data-selected="value === option.value"
+        >
+          {{ option.label }}
+        </button>
       </li>
     </ul>
   </div>
