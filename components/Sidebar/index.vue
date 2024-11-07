7b0d14fa7806f78a09f881f2e37fd69ca5fd4ef4
Dicki Maulana <bukandicki@gmail.com>
2024-11-07 14:22:23 +0700

    style: finished up sidebar styling

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,7 +1,49 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {
+  LazyIconChart,
+  LazyIconFolder,
+  LazyIconGear,
+  LazyIconHome,
+} from "#components";
+import { MENUS } from "~/lib/constants";
+
+const ICONS: Record<"home" | "folder" | "chart" | "gear", Component> = {
+  home: LazyIconHome,
+  folder: LazyIconFolder,
+  chart: LazyIconChart,
+  gear: LazyIconGear,
+};
+
+const route = useRoute();
+</script>
 
 <template>
-  <aside class="Sidebar"></aside>
+  <aside class="Sidebar">
+    <div class="Sidebar__brand">
+      <LazyIconBrand />
+      <span>BeLaundry</span>
+    </div>
+
+    <div class="Sidebar__menu">
+      <span>Menu</span>
+
+      <ol class="Menu__items">
+        <li class="Menu__item" v-for="menu in MENUS">
+          <NuxtLink
+            :class="[
+              'Menu__link',
+              { 'Menu__link--active': route.path === menu.href },
+            ]"
+            :href="menu.href"
+          >
+            <component :is="ICONS[menu.icon]"></component>
+
+            <span>{{ menu.title }}</span>
+          </NuxtLink>
+        </li>
+      </ol>
+    </div>
+  </aside>
 </template>
 
 <style lang="scss">
