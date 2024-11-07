dcb2d69cbcc149733e18ff464df49c188e23c8c3
Dicki Maulana <bukandicki@gmail.com>
2024-11-07 17:04:54 +0700

    feat: init select option component

---
+++ components/Select/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup generic="T">
+const props = defineProps<{
+  placeholder?: string;
+  value: T;
+  options: Array<{ label: string; value: T }>;
+}>();
+</script>
+
+<template>
+  <div class="Select">
+    <button class="Select__trigger">
+      {{ placeholder ? placeholder : "Select option" }}
+    </button>
+
+    <ul class="Select__options">
+      <li
+        v-for="option in options"
+        class="Select__item"
+        :key="(option.value as string)"
+      >
+        <button>{{ option.label }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./Select.styles.scss" as *;
+</style>
