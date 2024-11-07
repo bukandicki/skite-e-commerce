fb2bdc49e2d6d307d5549c461c16407947ce9bd1
Dicki Maulana <bukandicki@gmail.com>
2024-11-07 17:43:11 +0700

    improve select component

--- components/Select/index.vue
+++ components/Select/index.vue
@@ -1,19 +1,30 @@
 <script lang="ts" setup generic="T">
 const props = defineProps<{
   placeholder?: string;
-  value: T;
+  value: T | undefined;
   options: Array<{ label: string; value: T }>;
 }>();
 
 const emit = defineEmits<{ change: [value: T] }>();
 
 const show = ref<boolean>(false);
+
+const getValueLabel = computed(() => {
+  const selected_option = props.options.find((op) => op.value === props.value);
+
+  if (selected_option) return selected_option.label;
+  else return props.placeholder;
+});
 </script>
 
 <template>
   <div :class="['Select', { 'Select--show': show }]">
     <button @click="show = !show" class="Select__trigger">
-      <span>
+      <span v-if="value">
+        {{ getValueLabel }}
+      </span>
+
+      <span v-else>
         {{ placeholder ? placeholder : "Select option" }}
       </span>
 
