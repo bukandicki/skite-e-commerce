813e1b7af60915ae290b49b664b8fea3ca167477
Dicki Maulana <bukandicki@gmail.com>
2024-11-08 07:54:29 +0700

    backend upload integration

--- pages/admin/products/create/index.vue
+++ pages/admin/products/create/index.vue
@@ -1,32 +1,61 @@
 <script lang="ts" setup>
 import { FAKE_CATEGORY_RESPONSE } from "~/lib/constants";
+import type { ProductType } from "~/lib/types";
 
 definePageMeta({
   name: "AdminCreateProductPage",
   layout: "admin",
 });
 
-const selectedImage = ref<string | null>(null);
+const productStore = useProductStore();
+const categoryStore = useCategoryStore();
+
+const selectedImage = ref<string>("");
+const formElement = ref<HTMLFormElement | null>(null);
+const fileElement = ref<HTMLInputElement | null>(null);
 const selectedCategory = ref<number | null>(null);
+const payload = ref<Partial<ProductType>>({});
+
+await useAsyncData("categories", () => categoryStore.FETCH_ALL());
+const { status, execute } = await useAsyncData(
+  "create_product",
+  () => productStore.CREATE(payload.value),
+  {
+    immediate: false,
+  }
+);
+
+watch(status, (val) => {
+  if (val === "success") {
+    formElement.value?.reset();
+    selectedCategory.value = null;
+    payload.value = {};
+    selectedImage.value = "";
+  }
+});
 
 const handleSelectFile = () => {
-  const input_el = document.getElementById(
-    "image-file-input"
-  ) as HTMLInputElement;
-
-  input_el.click();
+  fileElement.value?.click();
 };
 
-const handleFileChange = (e: Event) => {
+const handleFileChange = async (e: Event) => {
   const el = e.target as HTMLInputElement;
 
-  const fr = new FileReader();
+  const data = await new Promise<string>((resolve, reject) => {
+    const fr = new FileReader();
 
-  fr.onload = function () {
-    selectedImage.value = fr.result as string;
-  };
+    fr.onloadend = function () {
+      resolve(fr.result as string);
+    };
 
-  fr.readAsDataURL((el.files as FileList)[0]);
+    fr.onerror = function () {
+      reject();
+    };
+
+    fr.readAsDataURL((el.files as FileList)[0]);
+  });
+
+  selectedImage.value = data;
 };
 
 const handleCreateProduct = (e: Event) => {
@@ -35,6 +64,18 @@ const handleCreateProduct = (e: Event) => {
   const el = e.target as HTMLFormElement;
 
   const fd = new FormData(el);
+
+  payload.value = {
+    name: fd.get("name") as string,
+    description: fd.get("description") as string,
+    sku: fd.get("sku") as string,
+    stock: Number(fd.get("stock")),
+    category_id: selectedCategory.value as number,
+    price: Number(fd.get("price")),
+    image: selectedImage.value as string,
+  };
+
+  execute();
 };
 </script>
 
@@ -43,7 +84,7 @@ const handleCreateProduct = (e: Event) => {
     <section class="CreatePage__form">
       <h2>Add New Product</h2>
 
-      <form class="ProductForm" @submit="handleCreateProduct">
+      <form ref="formElement" class="ProductForm" @submit="handleCreateProduct">
         <LazyInput type="text" label="Product Name" name="name" />
         <LazyTextarea label="Description" name="description" />
 
@@ -57,7 +98,8 @@ const handleCreateProduct = (e: Event) => {
 
           <div>
             <button
-              v-for="category in FAKE_CATEGORY_RESPONSE.response"
+              type="button"
+              v-for="category in categoryStore.categories"
               :key="category.id"
               :data-selected="selectedCategory === category.id"
               @click="selectedCategory = category.id"
@@ -70,7 +112,9 @@ const handleCreateProduct = (e: Event) => {
         <div class="ProductForm__group">
           <LazyInput type="number" label="Price" name="price" />
 
-          <button type="submit">Publish</button>
+          <button type="submit" :disabled="status === 'pending'">
+            {{ status === "pending" ? "Publishing..." : "Publish" }}
+          </button>
         </div>
       </form>
     </section>
@@ -79,7 +123,7 @@ const handleCreateProduct = (e: Event) => {
       <input
         @change="handleFileChange"
         accept="image/*"
-        id="image-file-input"
+        ref="fileElement"
         type="file"
       />
 
